98e5cd3b1148588fb6fca6e7b4f7d4428ec4d5ba
Madison Abegglen <madison.abegglen@gmail.com>
2018-10-31 11:49:31 -0600

    some changes

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 let options = [
-    {
-        name: "rock",
-        number: 0,
-        beats: 2
-    }, 
-    {
-        name: "paper",
-        number: 1,
-        beats: 0
-    },
-    {
-        name: "scissors",
-        number: 2,
-        beats: 1
-    }
+  {
+    name: "rock",
+    number: 0,
+    beats: 2
+  },
+  {
+    name: "paper",
+    number: 1,
+    beats: 0
+  },
+  {
+    name: "scissors",
+    number: 2,
+    beats: 1
+  }
 ]
 
 
 function play(num) {
-    let computerChoice = options[Math.floor(Math.random()*3)];
-    let playerChoice = options[num];
-    let result = `${playerChoice.name} vs ${computerChoice.name} = `
+  let computerChoice = options[Math.floor(Math.random() * 3)];
+  let playerChoice = options[num];
+  let result = `${playerChoice.name} vs ${computerChoice.name} = `
 
-    if(playerChoice.name == computerChoice.name) {
-        result += "Tie"
-    }
-    else if(playerChoice.beats == computerChoice.number) {
-        result += "Win"
-    }
-    else {
-        result += "Lose"
-    }
-    document.getElementById('result').textContent = result;
+  if (playerChoice.name == computerChoice.name) {
+    result += "It's a Tie"
+  }
+  else if (playerChoice.beats == computerChoice.number) {
+    result += "You Win"
+  }
+  else {
+    result += "You Lose"
+  }
+  document.getElementById('result').textContent = result;
 };
 
 
